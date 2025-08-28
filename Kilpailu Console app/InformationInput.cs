dc2dd24e0631dcab799447c3ac71db9fff96b996
matiasVTK <matias.kajanne@student.eduko.fi>
2025-08-28 12:16:26 +0300

    Preliminary implementation

--- Kilpailu Console app/InformationInput.cs
+++ Kilpailu Console app/InformationInput.cs
@@ -1,1 +1,33 @@
-Console.WriteLine("Ah, shit, here we go again.");
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InformationInput
+{
+    internal class Program
+    {
+        public static string InformationInput()
+        {
+            string name;
+            string sport;
+            string points;
+            
+            Console.WriteLine("Please input your name:");
+            name = Console.ReadLine();
+
+            Console.Clear();
+
+            Console.WriteLine("Please input your sport:");
+            sport = Console.ReadLine();
+
+            Console.Clear();
+
+            Console.WriteLine("Please input your points:");
+            points = Console.ReadLine();
+
+            return $"{name} {sport} {points}";
+        }
+    }
+}
